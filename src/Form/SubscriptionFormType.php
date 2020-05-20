33a2d9dc057fff2c5da1e2d6518f0cacb018a843
Brice Thrower <bthrower@vortexglobal.com>
2020-05-20 09:24:32 -0500

    Add HTML Validation to Subscription page

--- src/Form/SubscriptionFormType.php
+++ src/Form/SubscriptionFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Member;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,19 @@ class SubscriptionFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('firstName')
-            ->add('lastName')
-        ;
+            ->add('firstName', TextType::class, [
+                'attr' => [
+                    'oninvalid' => "this.setCustomValidity('We need your First Name Please.')",
+                    'oninput' => "setCustomValidity('')",
+                ]
+            ])
+            ->add('lastName', TextType::class, [
+                'attr' => [
+                    'oninvalid' => "this.setCustomValidity('We need your Last Name Please.')",
+                    'oninput' => "setCustomValidity('')",
+                ]
+            ])
+            ->add('email', EmailType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
