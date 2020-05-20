a7251a0a53925009aa42357b0b184017bee8905a
Brice Thrower <bthrower@vortexglobal.com>
2020-05-20 16:31:17 -0500

    Profile Page Started

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class MainController extends AbstractController
 {
@@ -14,6 +16,18 @@ class MainController extends AbstractController
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @IsGranted("ROLE_USER")
+     * @Route("/profile", name="app_profile")
+     */
+    public function profile(Security $security)
+    {
+        $member = $security->getUser();
+        return $this->render('main/profile.html.twig', [
+            'member' => $member,
         ]);
     }
 }
